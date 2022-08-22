a459e260e3143387257f26820ef7118742de18b2
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-08-22 11:22:00 +0700

    feat: complete test1

--- logictest/test1.js
+++ logictest/test1.js
@@ -1,9 +1,19 @@
 let words = "beever";
 
-// Buatlah skema logika untuk memuat kata diatas menjadi berbentuk seperti berikut : 
+// Buatlah skema logika untuk memuat kata diatas menjadi berbentuk seperti berikut :
 // b
 // be
 // bee
 // beev
 // beeve
 // beever
+
+let output = "";
+for (let i = 1; i <= words.length; i++) {
+  for (let j = 0; j < i; j++) {
+    output += words[j];
+  }
+  output += "\n";
+}
+
+console.log(output);
