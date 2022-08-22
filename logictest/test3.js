6c193f48d114d54e084bc117cd701abb0aac1b80
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-08-22 12:35:16 +0700

    feat: completed test3

--- logictest/test3.js
+++ logictest/test3.js
@@ -23,35 +23,84 @@ OUTPUT:
 */
 
 function jumlahTabungan(listHarga, history) {
-    // Write your code here
+  // Write your code here
+  const tabungan = {};
+  const uangSaku = 10000;
+  const hari = [];
+  let belanja = [];
+  const data = history.split(".");
+
+  function getharga(harga, barang) {
+    const jajan = harga.filter((item) => {
+      return item.nama === barang;
+    })[0];
+    return jajan.harga;
+  }
+
+  //membagi item belanja dan hari
+  data.map((item) => {
+    const split = item.split("-");
+    hari.push(split[0]);
+    belanja.push(split[1]);
+    return [hari, belanja];
+  });
+  belanja = belanja.map((item) => {
+    const split = item.split(",");
+    return split;
+  });
+
+  //menghitung sisa uang saku
+  const sisaUangSaku = belanja.map(
+    (item) =>
+      uangSaku -
+      item
+        .map((i) => {
+          return getharga(listHarga, i);
+        })
+        .reduce((a, b) => {
+          return a + b;
+        })
+  );
+
+  //menghitung tabungan per hari
+  hari.map((day, index) => {
+    return (tabungan[day] = sisaUangSaku[index]);
+  });
+
+  //menghitung total tabungan
+  tabungan.TotalTabungan = sisaUangSaku.reduce((a, b) => {
+    return a + b;
+  });
+
+  return tabungan;
 }
 
 var hargaMakanan = [
   {
     nama: "ayam",
-    harga: 5000
+    harga: 5000,
   },
   {
     nama: "nasi",
-    harga: 2000
+    harga: 2000,
   },
   {
     nama: "cola",
-    harga: 1000
+    harga: 1000,
   },
   {
     nama: "chiki",
-    harga: 1500
+    harga: 1500,
   },
   {
     nama: "hotdog",
-    harga: 3000
+    harga: 3000,
   },
   {
     nama: "aqua",
-    harga: 2000
-  }
-]
+    harga: 2000,
+  },
+];
 
-var historyPembelian = `Senin-ayam,nasi,cola.Selasa-chiki,hotdog.Rabu-ayam,chiki.Kamis-hotdog.Jumat-chiki,cola,nasi`
-console.log(jumlahTabungan(hargaMakanan, historyPembelian))
+var historyPembelian = `Senin-ayam,nasi,cola.Selasa-chiki,hotdog.Rabu-ayam,chiki.Kamis-hotdog.Jumat-chiki,cola,nasi`;
+console.log(jumlahTabungan(hargaMakanan, historyPembelian));
